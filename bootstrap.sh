352fafd013faf201846bd4506e7dadb37230fda4
Bowsse <jaakko.poyhonen@gmail.com>
2016-06-07 14:02:06 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,21 @@
+#!/bin/bash
+
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export LANGUAGE="en_US.UTF-8"
 
+# RobotFramework
 sudo apt-get update
 sudo apt-get install -y python-pip git
 sudo pip install robotframework
-sudo pip install robotframework-extendedselenium2library
+sudo pip install robotframework-selenium2library
 sudo apt-get install -y firefox
 
+#  vnc4server for virtual display
 sudo apt-get install -y vnc4server
 
-git clone https://github.com/iotitude/Tests    tests
+# expect command for test script
+sudo apt-get install -y expect
+
+# clone test cases
+git clone https://github.com/IoTitude/Tests.git   test
